d50c30e8a815dd58e3cf7c6d6c71dbc157267a24
DamirUakhit <damiruakhit6767@gmail.com>
2022-06-03 01:26:48 -0400

    3 files modified, 7 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,15 +5,23 @@ import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
-import { Footer } from './footer/footer.component';
+import { FooterComponent } from './footer/footer.component';
+import { BodyComponent } from './body/body.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 @NgModule({
   imports: [
     BrowserModule,
     ReactiveFormsModule,
-    RouterModule.forRoot([{ path: '', component: AppComponent }]),
+    RouterModule.forRoot([{ path: '', component: BodyComponent }]),
   ],
-  declarations: [AppComponent, TopBarComponent, Footer],
+  declarations: [
+    AppComponent,
+    TopBarComponent,
+    FooterComponent,
+    BodyComponent,
+    SidebarComponent,
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
